d1d144e365f03a0a1f05ad02222c55e8c6ab4944
Grant Pidwell <grantpidwell@gmail.com>
2017-01-12 12:54:27 +0200

    Added error count to results; created a provider for the utils to enable easier modular testing of processor

--- test/unit/parser_tests.js
+++ test/unit/parser_tests.js
@@ -8,23 +8,23 @@ describe("parser-test", function () {
 
     var self = this;
 
-    var Parser = require('../lib/utils/parser');
+    var Parser = require('../../lib/utils/parser');
     this.__parser = new Parser();
 
-    fs.readFile(__dirname + path.sep + 'fixtures' + path.sep + 'tracey-sample.json', 'utf8', function (err, data) {
+    fs.readFile(path.join(__dirname, '..', path.sep, 'fixtures', path.sep, 'tracey-sample.json'), 'utf8', function (err, data) {
 
       if (err)
         return callback(err);
 
       self.__testData = JSON.parse(data);
 
-      fs.readFile(__dirname + path.sep + 'fixtures' + path.sep + 'single-test.json', 'utf8', function (err, data) {
+      fs.readFile(path.join(__dirname, '..', path.sep, 'fixtures', path.sep, 'single-test.json'), 'utf8', function (err, data) {
         if (err)
           return callback(err);
 
         self.__singleTestData = JSON.parse(data);
 
-        fs.readFile(__dirname + path.sep + 'fixtures' + path.sep + 'single-test-illegal-chars.json', 'utf8', function (err, data) {
+        fs.readFile(path.join(__dirname, '..', path.sep, 'fixtures', path.sep, 'single-test-illegal-chars.json'), 'utf8', function (err, data) {
           if (err)
             return callback(err);
 
@@ -42,10 +42,10 @@ describe("parser-test", function () {
 
     var parsedResult = this.__parser.parse(this.__singleTestData);
 
-    assert.equal(parsedResult[0].name, 'repo.happner-2.0-endpoint-service.requires and initializes the endpoint service, config with no endpoints');
-    assert.equal(parsedResult[0].value, 18);
-
     console.log(JSON.stringify(parsedResult));
+
+    assert.equal(parsedResult[1].name, 'repo.happner-2.results.0-endpoint-service.requires and initializes the endpoint service, config with no endpoints');
+    assert.equal(parsedResult[1].value, 18);
 
     callback();
   });
@@ -56,7 +56,7 @@ describe("parser-test", function () {
 
     var parsedResult = this.__parser.parse(this.__singleTestIllegalCharsData);
 
-    assert.equal(parsedResult[0].name, 'repo.happner-2.0-endpoint-service.requires and __ initializes the endpoint service __ config with no endpoints __');
+    assert.equal(parsedResult[0].name, 'repo.happner-2.results.0-endpoint-service.requires and __ initializes the endpoint service __ config with no endpoints __');
     assert.equal(parsedResult[0].value, 18);
 
     console.log(JSON.stringify(parsedResult));
@@ -68,7 +68,8 @@ describe("parser-test", function () {
 
     var parsedResult = this.__parser.parse(this.__testData);
 
-    assert.equal(parsedResult.length, this.__testData.aggregated.tests.count);
+    assert.equal(parsedResult[0].value, this.__testData.aggregated.tests.statuses.failed);
+    assert.equal(parsedResult.length, this.__testData.aggregated.tests.count + 1);
 
     callback();
   });
